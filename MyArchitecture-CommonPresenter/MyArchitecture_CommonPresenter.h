fceeedf9c6b9aa2e7dd6d9566338be431ccd16b9
김낙진 <nakjin@mail.shinhan.dev>
2022-02-19 22:04:25 +0900

    Initial Commit

---
+++ MyArchitecture-CommonPresenter/MyArchitecture_CommonPresenter.h
@@ -0,0 +1,18 @@
+//
+//  MyArchitecture_CommonPresenter.h
+//  MyArchitecture-CommonPresenter
+//
+//  Created by 60067670 on 2022/02/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyArchitecture_CommonPresenter.
+FOUNDATION_EXPORT double MyArchitecture_CommonPresenterVersionNumber;
+
+//! Project version string for MyArchitecture_CommonPresenter.
+FOUNDATION_EXPORT const unsigned char MyArchitecture_CommonPresenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyArchitecture_CommonPresenter/PublicHeader.h>
+
+
